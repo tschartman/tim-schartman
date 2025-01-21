685e5e6a1f4c680adf97ba2897a446507a5c6d29
tim <tschartman@yahoo.com>
2025-01-20 21:55:40 -0600

    Grammar changes

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -4,7 +4,7 @@ const timelineData = [
   {
     title: "Application Developer (IC2)",
     description:
-      "Joined Oracle as Application Developer. Built and maintained the departments core platform tools including a configuration service that is used by utility companies to configure their products. Worked on database migrations, backend and frontend code for various webservices and libraries that communicate with the platform.",
+      "Joined Oracle as an Application Developer. Built and maintained the department's core platform tools, including a configuration service used by utility companies to configure their products.  Worked on database migrations, backend and frontend code for various web services and libraries that communicate with the platform.",
     period: "2022 - Current",
     company: 'Oracle',
     skills: ['React', 'NodeJS', 'MySQL', 'Jenkins', 'Knex', 'Sequelize', 'Ruby', 'Docker']
@@ -12,7 +12,7 @@ const timelineData = [
   {
     title: "Senior Associate Application Developer",
     description:
-      "Began my first Job as an Associate Software Developer at Discover. Worked on a team that built and mainted a large call center application uising React and NodeJs. Got familiar with some new technology including GraphQL, Java Spring boot and microservice infastructure. Learned and implented micro front ends that could be used in multile applications as well as helped maintain a custom UI library similar to that of Material UI.",
+      "Started my career as an Associate Software Developer at Discover. Worked on a team that built and maintained a large call center application using React and NodeJs. Gained experience with new technologies such as GraphQL, Java Spring Boot, and microservice infrastructure. Learned and implemented micro frontends for use across multiple applications, and assisted in maintaining a custom UI library similar to Material UI.",
     period: "2020 - 2022",
     company: 'Discover',
     skills: ['React', 'NodeJS', 'GraphQL', 'Java', 'Jenkins', 'Springboot', 'Microservices']
@@ -20,7 +20,7 @@ const timelineData = [
   {
     title: "Discover Code Orange",
     description:
-      "Joined a program hosted by Discover through NIU called code_orange which paired teams of computer science students together to work on various projects. Worked on muliple interesting projects using a wide variety of Web frameworks inclucidne NodeJs, Angular, and React.",
+      "Joined a program hosted by Discover in partnership with NIU called code_orange, which paired teams of computer science students to collaborate on various projects. Contributed to multiple exciting projects, utilizing a diverse range of web frameworks, including NodeJs, Angular, and React. ",
     period: "2019 - 2020",
     company: 'Discover',
     skills: ['Angular', 'React', 'NodeJS', 'WebSockets', 'Machine Learning']
@@ -28,7 +28,7 @@ const timelineData = [
   {
     title: "First Internship",
     description:
-      "Software Developer Intern at Stratus Networks. Helped work on a website that controlled orders, customer accounts, and fiber details for a small internet company. Created a standalone app to import data from a large excel spreadsheet into a database that could then be easily visualized and searched through a React UI.",
+      "Software Developer Intern at Stratus Networks. Contributed to the development of a website that managed orders, customer accounts, and fiber details for a small internet service provider. Created a standalone application to import data from a large Excel spreadsheet into a database, enabling efficient data visualization and search through a React UI.",
     period: "2018 - 2019",
     company: 'Stratus Networks',
     skills: ['Java', 'React', 'NodeJS', 'Python']
@@ -36,7 +36,7 @@ const timelineData = [
   {
     title: "Started College",
     description:
-      "Studied Computer Science at Northern Illinois University. Coursework focused on learning C/C++ by implementign commonly used data structures like heaps and maps. Studied Assemble language using IBM System/360.",
+      "Studied Computer Science at Northern Illinois University, with coursework focused on learning C/C++ by implementing commonly used data structures such as heaps and maps. Also studied assembly language using the IBM System/360. ",
     period: "2016 - 2019",
     company: 'Northern Illinois University',
     skills: ['C', 'C++', 'Assembly', 'Java', 'Javascript']
